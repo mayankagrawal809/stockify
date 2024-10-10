bdba3590ec715f7d27df17f8e6401d36addce716
Mayank Agrawal <mayankagrawal809@gmail.com>
2024-10-11 00:02:44 +0900

    Refactor authentication logic in router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,7 +4,6 @@ import LoginView from '@/views/LoginView.vue'
 
 function isAuthenticated(): boolean {
   const token = localStorage.getItem('jwt_token');
-  console.log('Token:', token);
   return !!token;
 }
 const router = createRouter({
@@ -22,17 +21,32 @@ const router = createRouter({
       name: 'login',
       component: () => LoginView
     },
-
+    // Wildcard route to catch all undefined routes
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      redirect: () => {
+        if (isAuthenticated()) {
+          return { name: 'home' };  // Redirect to home if authenticated
+        } else {
+          return { name: 'login' }; // Redirect to login if not authenticated
+        }
+      },
+      meta: { requiresAuth: false }
+    },
   ]
 })
 
 router.beforeEach((to, from, next) => {
+  const authenticated = isAuthenticated();
 
-  if (to.name === 'login' && isAuthenticated()) {
-    next({ name: 'home' });
-  } else if (!isAuthenticated() && to.name !== 'login') {
+  if (to.meta.requiresAuth && !authenticated) {
     next({ name: 'login' });
-  } else {
+  }
+  else if (authenticated && to.name === 'login') {
+    next({ name: 'home' });
+  }
+  else {
     next();
   }
 });
