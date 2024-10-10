b973be0f0b1d89bdd454cc479faf83ef4e25430f
Mayank Agrawal <mayankagrawal809@gmail.com>
2024-10-10 23:28:52 +0900

    Add auth middleware in backend

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,13 @@ const JWT_SECRET = '6F4D26CD5CCB494E8918D137C3CD5'; // Should store in .env file
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const users = [];
+const supportedStocks = ['GOOG', 'TSLA', 'AMZN', 'META', 'NVDA'];
 
 async function initializeUsers() {
     users.push({
         id: 1,
         username: 'admin',
-        password: await bcrypt.hash('admin', 10), // Hash with salt rounds of 10
+        password: await bcrypt.hash('admin', 10),
     });
     users.push({
         id: 2,
@@ -30,15 +31,7 @@ initializeUsers();
 app.use(cors());
 app.use(express.json());
 
-// Basic route
-app.get('/', authenticateToken, (req, res) => {
-    res.send('Hello World!');
-});
 
-app.get('/api/data', authenticateToken, (req, res) => {
-    console.log('Hello from the backend!');
-    res.json({ message: 'Hello from the backend!' });
-});
 
 
 app.post('/api/login', async (req, res) => {
@@ -55,11 +48,18 @@ app.post('/api/login', async (req, res) => {
         return res.status(401).json({ message: 'Login failed' });
     }
 
-    const token = jwt.sign({ userId: user.id, username: user.username }, JWT_SECRET, {
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
         expiresIn: '3h', // Token expires in 3 hours
     });
     res.json({ message: 'Login successful', token });
 });
+
+
+// Endpoint to get supported stocks
+app.get('/api/supported-stocks', authenticateToken, (req, res) => {
+    res.json({ stocks: supportedStocks });
+});
+
 // In-memory list of clients with their subscribed stock
 let clients = [];
 
@@ -82,7 +82,6 @@ app.get('/api/stock-updates/:ticker', (req, res) => {
     });
 });
 
-const supportedStocks = ['GOOG', 'TSLA', 'AMZN', 'META', 'NVDA'];
 function sendStockUpdates() {
     supportedStocks.forEach((ticker) => {
         const price = (Math.random() * 1000).toFixed(2);
@@ -97,7 +96,8 @@ function sendStockUpdates() {
 }
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
+
+    const authHeader = req.headers['authorization'] || req.headers['Authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
@@ -112,7 +112,7 @@ function authenticateToken(req, res, next) {
         next();
     });
 }
-setInterval(sendStockUpdates, 5000);
+setInterval(sendStockUpdates, 2000);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
